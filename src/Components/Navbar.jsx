2720c911e8db25a653f27deb9f96af01199697ce
Tushar-exe <patle.tushar2000@gmail.com>
2025-06-26 17:51:04 +0530

    fixed the navbar padding

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';  // Import Link from react-router-dom
 import '../Custom_css/Navbar.css'
 const Navbar = () => {
   return (
-    <nav className="navbar-fixed border-bottom px-3 py-4 navbar navbar-light bg-primary navbar-expand-lg ">
+    <nav className="navbar-fixed border-bottom px-3 py-2 navbar navbar-light bg-primary navbar-expand-lg ">
       <div className="container-fluid">
                 <button
           className="navbar-toggler ms-auto"
